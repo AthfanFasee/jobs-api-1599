eb8ade697c784a974c34bd9b4b6c0837c5882640
Athfan <aththaar47@gmail.com>
2022-01-31 07:25:18 +0530

    Final

--- app.js
+++ app.js
@@ -34,7 +34,9 @@ app.use(helmet())
 app.use(cors())
 app.use(xss())
 
-
+app.get('/', (req, res) => {
+  res.send('Jobs API')
+})
 // routes
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/jobs', authMiddleware, jobsRouter) //by using middleware here we can protect all of our jobs routes  
